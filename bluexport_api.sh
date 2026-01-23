dc1ee6fdef976e10e946db982be27248584d4f71
RQM <rqmartins73@gmail.com>
2026-01-23 16:22:47 +0000

    Fixes to flag -restorefromarchive

--- bluexport_api.sh
+++ bluexport_api.sh
@@ -672,16 +672,13 @@ cos_ls_buckets() {
 	curl -sX GET https://s3.$REGION.cloud-object-storage.appdomain.cloud/ -H "$header_auth" -H "ibm-service-instance-id: $SERVICE_INSTANCE_ID"
 }
 
-cos_rest_arch() {
+#cos_rest_arch() {
 cos_rest_arch() {
 	local bucket_name="$1"
 	local object_key="$2"
 	local actions_xml="$3"
 
-	curl -sX POST "https://s3.$REGION.cloud-object-storage.appdomain.cloud/$bucket_name/$object_key?restore" \
-		-H "$header_auth" \
-		-H "$header_xml" \
-		--data-binary "$actions_xml" 2>>"$log_file" | tee -a "$log_file"
+	curl -sX POST "https://s3.$REGION.cloud-object-storage.appdomain.cloud/$bucket_name/$object_key?restore" -H "$header_auth" -H "$header_xml" --data-binary "$actions_xml" 2>>"$log_file" | tee -a "$log_file"
 }
 
 #	curl -sX POST https://s3.$REGION.cloud-object-storage.appdomain.cloud/$BUCKET/$OBJECT?restore -H "$header_auth" -H "$header_json" -d "{$ACTIONS}"
