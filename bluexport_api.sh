fc0a10fe738a3f8b64f97562eaf8d5936773fc1b
RQM <rqmartins73@gmail.com>
2026-04-28 18:02:04 +0100

    Added flag -imgimport

--- bluexport_api.sh
+++ bluexport_api.sh
@@ -115,6 +115,7 @@
 # Note: Recurrence "hourly" and "daily" only permits captures to image-catalog.
 #
 # Ricardo Martins - Blue Chip Portugal - 2025-2026
+# IBM Champion 2025|2026
 ##########################################################################################
 # Ensure required tools are in PATH (IBM i + Linux safe)
 PATH="/QOpenSys/pkgs/bin:/QOpenSys/usr/bin:/usr/bin:${PATH}"
@@ -480,6 +481,7 @@ help() {
 	echoscreen "Note: Recurrence \"hourly\" and \"daily\" only permits captures to image-catalog."
 	echoscreen ""
 	echoscreen "Ricardo Martins - Blue Chip Portugal - 2025-2026"
+	echoscreen "IBM Champion 2025|2026"
 	echoscreen ""
 }
 #### END:FUNCTION - Help  ####
